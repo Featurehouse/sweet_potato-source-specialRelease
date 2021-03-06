d5d9487a6f07941d6aa7e24ee4a62fd80cbf0d1a
teddyxlandlee <tedexe_work.top@foxmail.com>
2021-03-06 22:13:42 +0800

    ClientWorldAccessor.kt -> ClientWorldAccessor.java

---
+++ src/main/java/org/featurehouse/spm/mixin/ClientWorldAccessor.java
@@ -0,0 +1,12 @@
+package org.featurehouse.spm.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ClientWorld.class)
+public interface ClientWorldAccessor {
+    @Accessor
+    MinecraftClient getClient();
+}
